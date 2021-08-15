877b26dd30dbfb1682c6ec1d73464937906bb994
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-08-15 18:42:19 +0530

    Initial Commit

---
+++ examples/intro/intro.py
@@ -0,0 +1,19 @@
+    """A Introduction to ShareImage and a working Example
+    """
+from ShareImage import ShareImage, clean_tags
+
+tags = [
+    'React',
+    'JSX',
+    'Markdown'
+]
+
+image = ShareImage(
+    title="ShareImage - A Python Library to Create Dynamic Share Images powered by Cloudinary",
+    cloudName="zype",
+    imagePublicId="CodeWithR/TEMPLATE",
+    tagline=clean_tags(tags),
+    taglineFont="futura"
+)
+
+print(image)
